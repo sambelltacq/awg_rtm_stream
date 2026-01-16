ded05d2a89fb71a0f7fb6c718ba5396fa095c00a
Peter Milne <peter.milne@d-tacq.com>
2026-01-16 10:26:03 +0000

    play_loop_ao42[24].sh [UUT [DELAY=1]]  On branch main 	modified:   play_loop_ao422.sh 	modified:   play_loop_ao424.sh

--- play_loop_ao422.sh
+++ play_loop_ao422.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 UUT=${1:-acq1001_315}
+DELAY=${2:-1}
 while true; do
     for file in ao422_patterns/*; do
         cat $file
-	sleep 1
+	sleep $DELAY
     done
 done | pv | nc $UUT 54207
--- play_loop_ao424.sh
+++ play_loop_ao424.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 UUT=${1:-acq1001_084}
+DELAY=${2:-1}
 
 while true; do
     for file in ao424_patterns/*; do
         cat $file
-	sleep 1
+	sleep $DELAY
     done
 done | pv | nc $UUT 54207
