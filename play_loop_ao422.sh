fa5a29a5ed0cde7e686cd16e81e02677499351ec
Peter Milne <peter.milne@d-tacq.com>
2026-01-16 11:59:56 +0000

    harden against "connect too early" @@todo needs to drop out on connection fail
    ** Example start while rebooting:
    (base) pgm@hoy6:~/SANDBOX/AWG_RTM/awg_rtm_stream$ ./play_loop_ao422.sh
    Ncat: TIMEOUT.
    STATE
    Ncat: TIMEOUT.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    Ncat: Connection refused.
    STATE
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc NOT READY
    STATE rclc UP acq1001_315 11:58:26 up 0 min, load average: 1.18, 0.31, 0.10
    UUT is ready!
    
    ** Finally, we're off
    43.6MiB 0:00:08 [6.97MiB/s] [            <=>                                                                  ]
    
    Ncat: Connection reset by peer.                  <=>                                                          ]
    
    ** @@ todo : trap connection reset by peer and drop out
    
    66.6MiB 0:00:22 [2.92MiB/s] [                       <=>
    
    	modified:   play_loop_ao422.sh

--- play_loop_ao422.sh
+++ play_loop_ao422.sh
@@ -2,6 +2,22 @@
 
 UUT=${1:-acq1001_315}
 DELAY=${2:-1}
+
+wait_for_uut_ready() {
+	while true; do
+		STATE=$(echo rc_local_complete | nc acq1001_315 4220)
+		echo STATE $STATE
+		#if echo $STATE | grep -q 'rclc acq...._... ..:..:.. up'; then
+		if echo $STATE | grep -q 'rclc UP acq...._... ..:..:.. up'; then
+			echo UUT is ready!
+			break
+		fi
+		sleep 1
+	done
+}
+
+wait_for_uut_ready
+
 while true; do
     for file in ao422_patterns/*; do
         cat $file
